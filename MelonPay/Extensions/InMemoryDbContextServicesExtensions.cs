e0236ce6872d500fcdc54f4fb042a16b44e7265b
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-12-21 12:55:12 +0300

    [+] ServicesExtensions : created in-memory services extensions

---
+++ MelonPay/Extensions/InMemoryDbContextServicesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using MelonPay.Abstractions;
+using MelonPay.DataSources;
+using MelonPay.DbContexts;
+using MelonPay.Entities;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class InMemoryDbContextServicesExtensions
+    {
+        public static IServiceCollection AddInMemoryServices(this IServiceCollection services)
+        {
+            services.AddSingleton<InMemoryDbContext>();
+            services.AddTransient<ICatalogueRepository<Account>, InMemoryAccountsRepository>();
+            services.AddTransient<ICardHolderRepository, InMemoryCardHolderRepository>();
+            services.AddTransient<IWalletRepository, InMemoryWalletRepository>();
+            services.AddTransient<IInvoiceRepository, InMemoryInvoiceRepository>();
+
+            return services;
+        }
+    }
+}
--- MelonPay/Startup.cs
+++ MelonPay/Startup.cs
@@ -25,6 +25,7 @@ namespace MelonPay
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInMemoryServices();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
