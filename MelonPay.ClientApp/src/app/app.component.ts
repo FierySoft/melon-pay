90c6f87f0373f9f2bc692f3bd49f6e04364ef73b
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-12-25 12:58:25 +0300

    [-] AppComponent : removed unused invoicesSource$

--- MelonPay.ClientApp/src/app/app.component.ts
+++ MelonPay.ClientApp/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { ApiService } from './api.service';
-import { IDelegate, CardHolder, InvoicesReport } from './app.models';
+import { CardHolder, InvoicesReport } from './app.models';
 
 @Component({
     selector: 'app-root',
@@ -11,7 +11,6 @@ import { IDelegate, CardHolder, InvoicesReport } from './app.models';
 })
 export class AppComponent {
     cardHolders$: Observable<CardHolder[]>;
-    invoicesSource$: IDelegate<InvoicesReport>;
     invoices$: Observable<InvoicesReport>;
 
     visible: boolean = false;
@@ -19,12 +18,11 @@ export class AppComponent {
 
     constructor(private _api: ApiService) {
         this.cardHolders$ = this._api.getCardHolders();
-        this.invoicesSource$ = (id: number) => this.select(id);
     }
 
-    select = (cardHolderId: number): Observable<InvoicesReport> => {
+    select = (cardHolderId: number): void => {
         this.visible = true;
         this.selectedId = cardHolderId;
-        return this.invoices$ = this._api.getInvoicesFor(cardHolderId);
+        this.invoices$ = this._api.getInvoicesFor(cardHolderId);
     }
 }
