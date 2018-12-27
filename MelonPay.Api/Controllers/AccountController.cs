0cd3168b2319bee7149e2be2bca61108c3710d67
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-12-27 10:02:47 +0300

    [+] AccountController : created endpoint fot signed-in user account info

---
+++ MelonPay.Api/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelonPay.Api.Controllers
+{
+    using MelonPay.Api.Abstractions;
+
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> WhoAmI()
+        {
+            return Ok(await _userService.GetSignedInAsync());
+        }
+    }
+}
